a0f1c36ed4ee839fe2f0e897004c3f7bb2ceaa12
sumitgangwar-cedcommerce <sumitgangwar@cedcoss.com>
2024-03-07 18:30:10 +0530

    check3211

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,7 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  const a  = [
-  "sum" , "shiv" , "numbez" , "wrogn" , 
-  ]
-  // console.log("numbe")
+  
   return (
     <div className="App">
       Hello Shivom numbe
